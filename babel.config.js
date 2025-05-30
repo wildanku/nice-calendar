b3eadea57435f9ad51a11142a08082a259366df0
wildanmaulana <wildaanmaulana@gmail.com>
2025-05-30 21:23:04 +0800

    feat: env

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
 
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
-    plugins,
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+    ],
   };
 };
---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare module '@env' {
+  export const GOOGLE_CLIENT_ID: string;
+  export const GOOGLE_API_KEY: string;
+  export const OPENWEATHER_API_KEY: string;
+  export const UNSPLASH_ACCESS_KEY: string;
+  export const UNSPLASH_SECRET_KEY: string;
+  export const UNSPLASH_APPLICATION_ID: string;
+}
