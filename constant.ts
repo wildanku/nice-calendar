e83d2c18d35719066d9b66ac5be3d3a4596c28a1
wildanmaulana <wildaanmaulana@gmail.com>
2025-05-30 21:26:40 +0800

    Remove constant.ts from Git tracking for security

--- constant.ts
+++
@@ -1,9 +0,0 @@
-export const GOOGLE_CLIENT_ID = '';
-
-export const GOOGLE_API_KEY = '';
-export const OPENWEATHER_API_KEY = '';
-export const Unsplash = {
-  ACCESS_KEY: '',
-  SECRET_KEY: '',
-  APPLICATION_ID: '',
-};
